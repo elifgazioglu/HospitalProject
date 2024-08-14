287c93bc9fb6a8979116eb3f96111646c33dda04
FATİH TÜRKER AKPINAR <afatihturker@gmail.com>
2024-08-14 11:29:23 +0300

    created validation

---
+++ HospitalProject/Common/Validator/CommonValidation.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+using System.Reflection;
+
+public class CommonValidation<TRequest> : AbstractValidator<TRequest>
+{
+    public CommonValidation()
+    {
+        // Get the type of TRequest
+        var requestType = typeof(TRequest);
+
+        // Find the 'Id' property
+        var idProperty = requestType.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                                    .FirstOrDefault(p => p.Name == "Id");
+
+        if (idProperty != null)
+        {
+            // Use reflection to validate the property
+            RuleFor(r => r)
+                .Must(r =>
+                {
+                    var idValue = idProperty.GetValue(r);
+                    if (idValue == null) return false;
+
+                    // Ensure the value is greater than 0
+                    if (idValue is int intValue)
+                    {
+                        return intValue > 0;
+                    }
+
+                    // Handle other types if necessary
+                    return false;
+                })
+                .WithMessage("Id must be greater than 0.");
+        }
+    }
+}
+
+public class IntValidator : AbstractValidator<int>
+{
+    public IntValidator()
+    {
+        RuleFor(x => x)
+            .GreaterThan(0)
+            .WithMessage("Value must be greater than 0.");
+    }
+}
+
--- HospitalProject/Controllers/UserController.cs
+++ HospitalProject/Controllers/UserController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using api.Data;
+using HospitalProject.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HospitalProject.Controllers
 {
@@ -7,5 +11,41 @@ namespace HospitalProject.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly ApplicationDBContext _context;
+
+        public UserController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUser(int id)
+        {
+            var validation = new IntValidator();
+
+            var validationResult = validation.Validate(id);
+
+            if (validationResult != null)
+            {
+                return BadRequest(validationResult);
+            }
+
+            var user = _context.Users.Find(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
     }
 }
