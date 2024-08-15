b4b50ea9c57dd617c5486c6a5e2736db5d89baa9
Elif Gazioğlu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2024-08-15 15:09:31 +0300

    created department model

---
+++ HospitalProject/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HospitalProject.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; } = null!;
+
+        // Bir departmanın birden fazla doktoru olabilir.
+        public ICollection<Doctor> Doctors { get; set; } = new List<Doctor>();
+    }
+}
