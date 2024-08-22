9f5e893ad9f60da0a1361aa207639f3ddfab3158
Elif Gazioğlu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2024-08-22 10:09:40 +0300

    created protected controller for protection

---
+++ HospitalProject/Controllers/ProtectedController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProtectedController : ControllerBase
+    {
+        // Bu endpoint JWT ile kimlik doğrulama gerektirir.
+        [Authorize]
+        [HttpGet("protected")]
+        public ActionResult<string> GetProtected()
+        {
+            return "This is a protected resource";
+        }
+    }
+}
