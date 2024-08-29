fc0f3c055b7aed29415281c0b2cf9940b4264e8b
Enes Cengiz <161731240+Remonias@users.noreply.github.com>
2024-08-29 11:58:20 +0000

    Doctor Controllera departmant eklendi

---
+++ HospitalProject/Models/Enums/Department.cs
@@ -0,0 +1,13 @@
+namespace HospitalProject.Models.Enums
+{
+    public enum Department
+    {
+        Cardiology=1,
+        Radiology=2,
+        Oncology=3,
+        Neurology=4,
+        Psychiatry=5,
+        Dental=6,
+        Hematology=7
+    }
+}
